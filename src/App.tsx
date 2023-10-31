3c40f5896021eef4ac21c134e5c65cfbf93d02f2
Atlon1 <maciej.szajstek@gmail.com>
2023-10-31 21:04:19 +0100

    frist section prepere

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import '../src/styles/tailwind.css';
 import Layout from "./components/Layout";
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Work from "./pages/Work";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
 
 
 function App() {
@@ -10,6 +15,11 @@ function App() {
         <Layout>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/services'" element={<Services/>}/>
+                <Route path="/work" element={<Work/>}/>
+                <Route path="/testimonials" element={<Testimonials/>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
         </Layout>
 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,34 +8,47 @@ import {
     HiChatBubbleBottomCenterText,
     HiEnvelope,
 } from 'react-icons/hi2';
+import {Link} from 'react-router-dom';
 
 
-export const navData = [
-    { name: 'home', path: '/', icon: <HiHome /> },
-    { name: 'about', path: '/about', icon: <HiUser /> },
-    { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-    { name: 'work', path: '/work', icon: <HiViewColumns /> },
+type Link = {
+    name: string;
+    path: string;
+    icon: any;
+};
+export const navData: Link[] = [
+    {name: 'home', path: '/', icon: <HiHome/>},
+    {name: 'about', path: '/about', icon: <HiUser/>},
+    {name: 'services', path: '/services', icon: <HiRectangleGroup/>},
+    {name: 'work', path: '/work', icon: <HiViewColumns/>},
     {
         name: 'testimonials',
         path: '/testimonials',
-        icon: <HiChatBubbleBottomCenterText />,
+        icon: <HiChatBubbleBottomCenterText/>,
     },
     {
         name: 'contact',
         path: '/contact',
-        icon: <HiEnvelope />,
+        icon: <HiEnvelope/>,
     },
 ];
-
 
 
 const Nav = () => {
 
 
+
     return (
-        <div>
-            Nav
-        </div>
+        <nav>
+            <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4
+            md:px-40 xl:px-0 h-80 bg-red-200/10 xl:h-max py-8'>
+                {navData.map((link, index) => (
+                    <Link to={link.path} key={index}>
+                        {link.icon}
+                    </Link>
+                ))}
+            </div>
+        </nav>
     );
 };
 
