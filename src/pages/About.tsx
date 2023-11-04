b6f4c043a76b441db17ab399e1a6d14a8e5a4a60
Atlon1 <maciej.szajstek@gmail.com>
2023-11-04 23:37:56 +0100

    about rdy

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -35,10 +35,10 @@ const About = () => {
 
     const aboutData: AboutData[] = [
         {
-            title: 'skills',
+            title: 'Umiejętności',
             info: [
                 {
-                    title: 'Web Development',
+                    title: 'Projektowanie procesów',
                     icons: [
                         <FaHtml5/>,
                         <FaCss3/>,
@@ -56,7 +56,7 @@ const About = () => {
             ],
         },
         {
-            title: 'awards',
+            title: 'Doświadczenie',
             info: [
                 {
                     title: 'Webby Awards - Honoree',
@@ -69,24 +69,7 @@ const About = () => {
             ],
         },
         {
-            title: 'experience',
-            info: [
-                {
-                    title: 'UX/UI Designer - XYZ Company',
-                    stage: '2012 - 2023',
-                },
-                {
-                    title: 'Web Developer - ABC Agency',
-                    stage: '2010 - 2012',
-                },
-                {
-                    title: 'Intern - DEF Corporation',
-                    stage: '2008 - 2010',
-                },
-            ],
-        },
-        {
-            title: 'credentials',
+            title: 'Certyfikaty',
             info: [
                 {
                     title: 'Web Development - ABC University, LA, CA',
@@ -108,31 +91,73 @@ const About = () => {
     const [index, setIndex] = useState(0);
 
 
-
     return (
         <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
             <Circles/>
-            <motion.div
-                variants={fadeIn('right', 0.2)}
-                initial='hidden'
-                animate='show'
-                exit='hidden'
-                className='hidden xl:flex absolute bottom-0 -left-[370px]'>
-                <Avatar/>
-            </motion.div>
             <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
                 <div className='flex-1 flex flex-col justify-center'>
-                <h2 className='h2'>
-                    Captivating <span className='text-accent'>Stories</span> birth mgnificen designs.
-                </h2>
-                    <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <div>
-                        counters
-                    </div>
+                    <motion.h2
+                        variants={fadeIn('right', 0.2)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='h2'>
+                        Przerabiam <span className='text-accent'>Projekty</span> <br/> w rzeczywistość.
+                    </motion.h2>
+                    <motion.p
+                        variants={fadeIn('right', 0.4)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+                        Jestem doświadczonym inżynierem specjalizującym się w technologii i obróbce CNC, z
+                        umiejętnościami programowania różnych rodzajów obrabiarek. Moja praca polega na optymalizacji
+                        procesów produkcyjnych, redukcji czasu obróbki i minimalizacji marnotrawstwa materiału. Jako
+                        pasjonat technologii CNC, jestem gotów przyczynić się do sukcesu mojego przyszłego pracodawcy
+                        poprzez dostarczanie precyzyjnych komponentów i innowacyjnych rozwiązań.
+                    </motion.p>
+                    <motion.div
+                        variants={fadeIn('right', 0.6)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='hidden md:flex md:max-w-xl xl:max-w-none max-auto xl:mx-0 mb-24'>
+                        <div className='flex flex-1 xl:gap-x-6'>
+                            <div
+                                className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                                    <CountUp start={0} end={2} duration={5}/>
+                                </div>
+                                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                                    Lata doświadczenia
+                                </div>
+                            </div>
+                            <div
+                                className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                                    <CountUp start={0} end={15} duration={5}/>
+                                </div>
+                                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                                    Zadowoloni klienci
+                                </div>
+                            </div>
+                            <div className='relative flex-1 '>
+                                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                                    <CountUp start={0} end={12} duration={5}/>
+                                </div>
+                                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                                    Ukończone projekty
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>
                 </div>
-                <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+                <motion.div
+                    variants={fadeIn('left', 0.4)}
+                    initial='hidden'
+                    animate='show'
+                    exit='hidden'
+                    className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
                     <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
                         {aboutData.map((item, itemIndex) => {
                             return (
@@ -140,7 +165,7 @@ const About = () => {
                                      className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} 
                                 cursor-pointer capitalize xl:text-lg relative after:w-8
                                 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                                onClick={() => setIndex(itemIndex)}
+                                     onClick={() => setIndex(itemIndex)}
                                 >{item.title}</div>
 
                             )
@@ -148,27 +173,27 @@ const About = () => {
                     </div>
                     <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
                         {aboutData[index].info.map((item, itemIndex) => {
-                                return (
-                                    <div
-                                        className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-                                        key={itemIndex}>
-                                        <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-                                        <div className='hidden md:flex'>-</div>
-                                        <div>{item.stage}</div>
-                                        <div className='flex gap-x-4'>
-                                            {item.icons?.map((icon, iconIndex) => {
-                                                return (
-                                                    <div
-                                                        className='text-2xl transition-all text-white'
-                                                        key={iconIndex}>{icon}</div>
-                                                )
-                                            })}
-                                        </div>
+                            return (
+                                <div
+                                    className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
+                                    key={itemIndex}>
+                                    <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                                    <div className='hidden md:flex'>-</div>
+                                    <div>{item.stage}</div>
+                                    <div className='flex gap-x-4'>
+                                        {item.icons?.map((icon, iconIndex) => {
+                                            return (
+                                                <div
+                                                    className='text-2xl transition-all text-white'
+                                                    key={iconIndex}>{icon}</div>
+                                            )
+                                        })}
                                     </div>
-                                )
+                                </div>
+                            )
                         })}
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
