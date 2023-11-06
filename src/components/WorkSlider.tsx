5660a4c23694ac28a1acfd9b9ac75d3bc979af96
Atlon1 <maciej.szajstek@gmail.com>
2023-11-07 00:23:54 +0100

    section work prepere

--- src/components/WorkSlider.tsx
+++ src/components/WorkSlider.tsx
@@ -4,6 +4,10 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/free-mode';
 import {Pagination} from 'swiper';
+import thumb1 from '../assets/thumb1.jpg';
+import thumb2 from '../assets/thumb2.jpg';
+import thumb3 from '../assets/thumb3.jpg';
+import thumb4 from '../assets/thumb4.jpg';
 import {BsArrowRight} from 'react-icons/bs';
 
 const WorkSlider = () => {
@@ -14,19 +18,19 @@ const WorkSlider = () => {
                 images: [
                     {
                         title: 'title',
-                        path: '/thumb1.jpg',
+                        path: thumb1,
                     },
                     {
                         title: 'title',
-                        path: '/thumb2.jpg',
+                        path: thumb2,
                     },
                     {
                         title: 'title',
-                        path: '/thumb3.jpg',
+                        path: thumb3,
                     },
                     {
                         title: 'title',
-                        path: '/thumb4.jpg',
+                        path: thumb4,
                     },
                 ],
             },
@@ -34,19 +38,19 @@ const WorkSlider = () => {
                 images: [
                     {
                         title: 'title',
-                        path: '/thumb4.jpg',
+                        path: thumb4,
                     },
                     {
                         title: 'title',
-                        path: '/thumb1.jpg',
+                        path: thumb3,
                     },
                     {
                         title: 'title',
-                        path: '/thumb2.jpg',
+                        path: thumb2,
                     },
                     {
                         title: 'title',
-                        path: '/thumb3.jpg',
+                        path: thumb1,
                     },
                 ],
             },
@@ -61,12 +65,35 @@ const WorkSlider = () => {
                 clickable: true,
             }}
             modules={[Pagination]}
-            className='h-[240px] sm:h-[480px]'
+            className='w-full h-full'
         >
             {workSlides.slides.map((slide, index) => {
                 return (
                     <SwiperSlide key={index}>
-                        Slide
+                        <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+                            {slide.images.map((image, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                                        <div className='flex items-center justify-center relative overflow-hidden group'>
+                                            <img
+                                                className='max-w-[500px] max-h-[250px] h-full w-full'
+                                                src={image.path} alt={image.title}/>
+                                        </div>
+                                        <div
+                                            className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                                        <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 flex'>
+                                            <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                                                <div className='delay-100'>Live</div>
+                                                <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 deley-150'>Project</div>
+                                                <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight/></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </SwiperSlide>
                 )
             })}
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -3,7 +3,7 @@ import Bulb from "../components/Bulb";
 import Circles from "../components/Circles";
 
 import {motion} from "framer-motion";
-import { fadeIn } from "../variants";
+import {fadeIn} from "../variants";
 import WorkSlider from "../components/WorkSlider";
 
 
@@ -14,16 +14,32 @@ const Work = () => {
             <div className='container mx-auto'>
                 <div className=' flex flex-col xl:flex-row gap-x-8'>
                     <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-                        <h2 className='h2 xl:mt-8'>
+                        <motion.h2
+                            variants={fadeIn('up', 0.2)}
+                            initial='hidden'
+                            animate='show'
+                            exit='hidden'
+                            className='h2 xl:mt-12'>
                             Moja praca<span className='text-accent'>.</span>
-                        </h2>
-                        <p className='mb-4 max-w-[400px] mx-auto lg:max-0'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
+                        </motion.h2>
+                        <motion.p
+                            variants={fadeIn('up', 0.4)}
+                            initial='hidden'
+                            animate='show'
+                            exit='hidden'
+                            className='mb-4 max-w-[400px] mx-auto lg:max-0'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </motion.p>
                     </div>
-                    <div className='w-full xl:max-w-[60%]'>
+                    <motion.div
+                        variants={fadeIn('down', 0.2)}
+                        initial='hidden'
+                        animate='show'
+                        exit='hidden'
+                        className='w-full xl:max-w-[60%]'>
                         <WorkSlider/>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
             <Bulb/>
