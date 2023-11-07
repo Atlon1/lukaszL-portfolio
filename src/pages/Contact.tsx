bc56d032633177956a8f6bc1ddb93d93bfd5e8e8
Atlon1 <maciej.szajstek@gmail.com>
2023-11-07 20:43:55 +0100

    contact section prepere

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {BsArrowRight} from "react-icons/bs";
+import {motion} from "framer-motion";
+import {fadeIn} from "../variants";
 
 const Contact = () => {
     return (
         <div>
-            Contact
+            <div>
+                <div>
+                    <h2>Skontkatuj sie ze mną</h2>
+                    <form>
+                        <div>
+                            <input type='text'/>
+                            <input type='text'/>
+                        </div>
+                        <input type='text'/>
+                        <textarea></textarea>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
