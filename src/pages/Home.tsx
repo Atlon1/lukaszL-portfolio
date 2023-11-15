2f12f66cce92c6ce798be4dd182a88ba506e15dd
Atlon1 <maciej.szajstek@gmail.com>
2023-11-15 22:26:02 +0100

    foto update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {fadeIn} from '../variants'
 import ParticlessCont from "../components/ParticlessCont";
 import {TypeAnimation} from "react-type-animation";
 import Header from "../components/Header";
+import Lukasz from "../assets/LukaszL.png"
 
 const Home = () => {
     return (
@@ -13,8 +14,7 @@ const Home = () => {
         <>
             <Header/>
             <div className='bg-primary/60 h-full overflow-auto xl:pt-0 pt-32'>
-
-                    <ParticlessCont/>
+                <ParticlessCont/>
                 <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
                     <div
                         className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
@@ -46,7 +46,7 @@ const Home = () => {
                             initial='hidden'
                             animate='show'
                             exit='hidden'
-                            className=' max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 '>
+                            className=' max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 z-20'>
                             Jestem osobą nastawioną na
                             rozwój osobisty. Zawsze chętnie
                             biorę udział w wszystkich
@@ -58,8 +58,16 @@ const Home = () => {
                             Chętnie dzielę się wiedzą oraz
                             doświadczeniem.
                         </motion.p>
-                        <div className='xl:hidden md:flex justify-center hidden relative mb-0 md:mb-12'>
+                        <motion.div
+                            variants={fadeIn('down', 0.3)}
+                            initial='hidden'
+                            animate='show'
+                            exit='hidden'
+                            className='xl:hidden md:flex justify-center hidden relative mb-0 md:mb-12'>
                             <ProjectBtn/>
+                        </motion.div>
+                        <div className='md:hidden sm:flex w-full h-full max-w-[300px] max-h-[200px] absolute bottom-[150px] right-2 z-0 opacity-50'>
+                            <img src={Lukasz} alt='lukasz'/>
                         </div>
                         <motion.div
                             variants={fadeIn('down', 0.4)}
@@ -91,6 +99,7 @@ const Home = () => {
                         transition={{duration: 1, ease: 'easeInOut'}}
                         className='w-full h-full max-w-[600px] max-h-[500px] absolute lg:bottom-[200px] lg:right-[8%]'>
                         <Avatar/>
+
                     </motion.div>
                 </div>
             </div>
